2de16d490f17483dad24cd5d5937183e37c7c78e
tlinux <115206437+MasterT-800@users.noreply.github.com>
2024-02-28 13:36:45 -0500

    Add button element New button creates gid size based on user input.

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 const container = document.querySelector('#container');
 
-// Create 256 boxes for 16 x 16 grid 
-for (let i = 0; i <16; i++) {
-    const column = document.createElement('div');
-    column.classList.add('column'); 
-    container.appendChild(column);
-    for (let j = 0; j <16; j++) {
-      const boxes = document.createElement('div');
-      boxes.classList.add('boxes'); 
-      column.appendChild(boxes);
+// Promt user for grid size and create grid based on input
+function createGrid(){
+  let size = 16;
+  do {
+    size = prompt('Grid Size?', 16);
+  } while (size > 100);
+  for (let i = 0; i <size; i++) {
+      const column = document.createElement('div');
+      column.classList.add('column'); 
+      container.appendChild(column);
+      for (let j = 0; j <size; j++) {
+        const boxes = document.createElement('div');
+        boxes.classList.add('boxes'); 
+        column.appendChild(boxes);
+      }
     }
-  }
+      // Change color of box div on mouse over
+  const boxes = document.querySelectorAll('.boxes')
 
-// Change color of box div on mouse over
-const boxes = document.querySelectorAll('.boxes')
+  // Iterate through nodelist
+  boxes.forEach((box) => {
 
-// Iterate through nodelist
-boxes.forEach((box) => {
-
-  // For each one add a 'mouseover' listener
-  box.addEventListener('mouseover', () => {
-    box.style.background = 'red';
+    // For each one add a 'mouseover' listener
+    box.addEventListener('mouseover', () => {
+      box.style.background = 'red';
+    });
   });
-});
+}
+
+
+// Button event asks user for grid size and creates grid based on input
+const btn = document.querySelector('button');
+btn.addEventListener('click', createGrid);
+
